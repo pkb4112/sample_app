657a26cadf160eb81882a8962b90a76ff8b7d308
Peter Brandon <peterkbrandon@gmail.com>
2017-12-01 19:35:30 -0500

    Finish layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,14 @@
 Rails.application.routes.draw do
+
+
+  
+
   root 'static_pages#home'
-  get 'static_pages/home'
-  get 'static_pages/about'
-  get 'static_pages/help'
-  get 'static_pages/contact'
+  get '/about', to: 'static_pages#about'
+  get '/help', to: 'static_pages#help'
+  get '/contact', to: 'static_pages#contact'
+  get '/login', to: 'static_pages#login'
+  get '/signup', to: 'users#new'
 
 
 
