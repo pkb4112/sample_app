0778fc3f7c62365f87af34ae9cdce5a550ac7012
Peter Brandon <peterkbrandon@gmail.com>
2018-01-05 10:46:17 -0500

    Add account activation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,12 @@
 class User < ApplicationRecord
-	before_save {email.downcase!}
+	before_save :downcase_email
+  before_create :create_activation_digest
 	validates :name, presence: true, length: {maximum: 50}
 	validates :email, presence: true, length: {maximum: 255}, format: {with: /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i}, uniqueness: {case_sensitive: false}
   has_secure_password
   validates :password, presence: true, length: {minimum: 6}, allow_nil: true
 
-  attr_accessor :remember_token
+  attr_accessor :remember_token, :activation_token
 
   #Returns hash digest of given string. 
   def self.digest(string)
@@ -19,7 +20,7 @@ class User < ApplicationRecord
   	SecureRandom.urlsafe_base64
   end
 
-  #Remembers a usser in the database for use in persistent sessions
+  #Remembers a user in the database for use in persistent sessions
 
   def remember
   	self.remember_token = User.new_token
@@ -27,14 +28,41 @@ class User < ApplicationRecord
   end
 
   #Returns true if the given token matches the digest. 
-  def authenticated?(remember_token)
-  	return false if remember_digest.nil?
-  	BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  def authenticated?(attribute, token)
+    digest = self.send("#{attribute}_digest")
+  	return false if digest.nil?
+  	BCrypt::Password.new(digest).is_password?(token)
   end
 
   #Forgets a user. Sets the remember digest to nil
   def forget
   	update_attribute(:remember_digest, nil)
   end
+
+  def activate
+    #update_attribute(:activated, true)
+    #update_attribute(:activated_at, Time.zone.now)
+    update_columns(activated: true, activated_at: Time.zone.now)
+  end
+
+  def send_activation_email
+    UserMailer.account_activation(self).deliver_now
+  end
+
+
+private 
+  
+  #Converts email to all lowercase
+ def downcase_email
+  self.email.downcase!
+ end
+
+ # Creates and assigns the activation token and digest. 
+ def create_activation_digest
+  self.activation_token = User.new_token
+  self.activation_digest = User.digest(activation_token)
+ end
+
+
 end
 
--- test/integration/users_index_test.rb
+++ test/integration/users_index_test.rb
@@ -33,6 +33,23 @@ class UsersIndexTest < ActionDispatch::IntegrationTest
   	assert_select 'a', text: 'delete', count: 0 
   end
 
+  test "show page should redirect if account not activated" do  
+    log_in_as(@admin)
+    @non_admin.update_attribute(:activated, false)
+    get user_path(@non_admin)
+    assert_redirected_to root_url
+    follow_redirect!
+    assert_template '/'
+  end
+
+  test "index page should not include users whose accounts are not activated" do  
+    log_in_as(@admin)
+    @non_admin.update_attribute(:activated, false)
+    get users_path
+    assert_template 'users/index'
+    assert_select 'a[href=?]', user_path(@non_admin), count: 0
+  end
+
 
 end
 
